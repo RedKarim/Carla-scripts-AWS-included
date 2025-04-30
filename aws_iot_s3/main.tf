32b64f52cefe6270f910212ecb82aa44c86a9ea4
RedKarim <rhlkr7474@gmail.com>
2025-04-30 11:02:37 +0900

    Add AWS Lambda function and IAM role for carla-platooning. Configure S3 trigger for Lambda and set up necessary permissions and environment variables.

--- aws_iot_s3/main.tf
+++ aws_iot_s3/main.tf
@@ -59,6 +59,109 @@ resource "aws_iot_topic_rule" "iot_to_s3" {
   }
 }
 
+# Lambda function configuration
+resource "aws_iam_role" "lambda_role" {
+  name = "carla-platooning-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "carla-platooning-lambda-policy"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "arn:aws:logs:*:*:*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject"
+        ],
+        Resource = "${aws_s3_bucket.gps_bucket.arn}/*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "iot:Publish"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Create Lambda function
+resource "aws_lambda_function" "platooning_lambda" {
+  filename         = "../function.zip"
+  function_name    = "carla-platooning-lambda"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.9"
+  timeout          = 30
+  memory_size      = 128
+
+  environment {
+    variables = {
+      DESIRED_DISTANCE = "5.0"
+      MAX_ACCELERATION = "1.5"
+      DESIRED_SPEED = "15.0"
+      FOLLOW_SPEED = "12.0"
+      TIME_HEADWAY = "1.0"
+      COMFORT_DECEL = "2.5"
+    }
+  }
+}
+
+# S3 trigger for Lambda
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.platooning_lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.gps_bucket.arn
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.gps_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.platooning_lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_suffix       = ".json"
+  }
+
+  depends_on = [aws_lambda_permission.allow_s3]
+}
+
 output "bucket_name" {
   value = aws_s3_bucket.gps_bucket.bucket
+}
+
+output "lambda_function_name" {
+  value = aws_lambda_function.platooning_lambda.function_name
+}
+
+output "lambda_function_arn" {
+  value = aws_lambda_function.platooning_lambda.arn
 }
